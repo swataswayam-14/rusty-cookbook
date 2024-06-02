8f8b0efb71d4918828b72c03b0103b707d3aa68b
Swata Swayam Dash <swataswayamdash@gmail.com>
2024-06-02 22:03:38 +0530

    modules and scope concept code added

---
+++ modules-rust/resturant/src/lib.rs
@@ -0,0 +1,25 @@
+mod front_of_house {
+    mod hosting {
+        fn add_to_waitlist(){
+
+        }
+
+        fn seat_at_table() {
+
+        }
+    }
+
+    mod serving{
+        fn take_order(){
+
+        }
+        fn serve_order(){
+
+        }
+        fn take_payment(){
+
+        }
+    }
+}
+
+//The module tree might remind you of the filesystem’s directory tree on your computer; this is a very apt comparison! Just like directories in a filesystem, you use modules to organize your code.
