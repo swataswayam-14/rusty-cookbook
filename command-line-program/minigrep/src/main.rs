83b724e8b4c7309b47005261fda9b4e9d2a556f7
Swata Swayam Dash <swataswayamdash@gmail.com>
2024-06-17 14:03:30 +0530

    env variables and improving error handleing in minigrep

--- command-line-program/minigrep/src/main.rs
+++ command-line-program/minigrep/src/main.rs
@@ -7,12 +7,12 @@ use minigrep::Config;
 fn main(){
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     }); //If the Result is an Ok value, this method’s behavior is similar to unwrap: it returns the inner value Ok is wrapping. However, if the value is an Err value, this method calls the code in the closure, which is an anonymous function we define and pass as an argument to unwrap_or_else.
 
     if let Err(e) = minigrep::run(config) { //here we have used if let rather than unwrap_or_else to check whether run returns an Err value and call process::exit(1)
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
     // The run function doesn’t return a value that we want to unwrap in the same way that Config::build returns the Config instance. Because run returns () in the success case, we only care about detecting an error, so we don’t need unwrap_or_else to return the unwrapped value, which would only be ().
